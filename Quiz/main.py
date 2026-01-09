60f23ab59be38cf7f055604845f53ef3a9854c26
Jiyansh Gokhru <jiyanshjain50@gmail.com>
2026-01-09 23:01:01 +0530

    Update main.py

--- Quiz/main.py
+++ Quiz/main.py
@@ -1,72 +1,53 @@
-print("Q1 = what's 2 + 5/2 + 5/2 " )
-print("""a) 6
-b) 7
-c) 10
-d) 5
+points = 0
+
+print("Q1. What is 2 + 5/2 + 5/2?")
+print("""A) 6
+B) 7
+C) 10
+D) 5
 """)
+ans = input("Choose the correct option: ").lower()
+if ans == "b":
+    points += 1
 
-points=0
-ans=input("Choose the correct option: ")
-print("")
-if ans=="b":
-    points+=1
-
-print("""Q2. What is 8 + 12 / 4?
-A) 5
+print("\nQ2. What is 8 + 12 / 4?")
+print("""A) 5
 B) 11
-C) 11 
-D) 20
+C) 20
+D) 8
 """)
+ans = input("Choose the correct option: ").lower()
+if ans == "b":
+    points += 1
 
-ans=input("Choose the correct option: ")
-print("")
-if ans=="c":
-    points+=1
-
-print("""Q3. What is the value of 3² + 4²?
-A) 25
-B) 25 
+print("\nQ3. What is the value of 3² + 4²?")
+print("""A) 7
+B) 25
 C) 49
 D) 12
 """)
+ans = input("Choose the correct option: ").lower()
+if ans == "b":
+    points += 1
 
-ans=input("Choose the correct option: ")
-print("")
-if ans=="b":
-    points+=1
-
-print("""Q4. What is 15 // 4 in Python?
-A) 3
-B) 3 
+print("\nQ4. What is 15 // 4 in Python?")
+print("""A) 3
+B) 4
 C) 3.75
-D) 4
+D) Error
 """)
+ans = input("Choose the correct option: ").lower()
+if ans == "a":
+    points += 1
 
-ans=input("Choose the correct option: ")
-print("")
-if ans=="b":
-    points+=1
-
-print("""Q5. What will len("Python") return?
-A) 5
-B) 6 
+print("\nQ5. What will len('Python') return?")
+print("""A) 5
+B) 6
 C) 7
 D) Error
 """)
+ans = input("Choose the correct option: ").lower()
+if ans == "b":
+    points += 1
 
-ans=input("Choose the correct option: ")
-print("")
-if ans=="b":
-    points+=1
-
-
-
-print(f"result: {points}/5")
-
-
-
-
-
-
-
-
+print(f"\nResult: {points}/5")
