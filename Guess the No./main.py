b1294bc35d32270849b0f5ff2491e4b0466f9387
Jiyansh Gokhru <jiyanshjain50@gmail.com>
2026-01-09 22:55:38 +0530

    Update main.py

--- Guess the No./main.py
+++ Guess the No./main.py
@@ -1,28 +1,21 @@
 import random
 
-num = random.randint(1,101)
-try:
-    guess=int(input("Guess the no. : "))
+num = random.randint(1, 101)
+attempts = 0
 
-    attempts = 1
+while True:
+    try:
+        guess = int(input("Guess the no. : "))
+        attempts += 1
 
-    while num!=guess:
-        
-        if num>guess:
-            print("Higher the number please! ")
-                    
+        if guess < num:
+            print("Higher the number please!")
+        elif guess > num:
+            print("Lower the number please!")
         else:
-            print("Lower the number please! ")
-                    
+            print("You guessed the number!")
+            print(f"Took you {attempts} attempts.")
+            break
 
-        guess=int(input("Guess the no. : "))
-        attempts+=1
-            
-    else:
-        print("You guessed the no.")
-        print(f"Took you {attempts} attempts.")
-
-except ValueError:
-        print("Invalid digit!")
-
-
+    except ValueError:
+        print("Invalid digit! Please enter a number.")
